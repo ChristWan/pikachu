5598da9f36630a0d3a18989a81867b76136cc10f
christwan <christyim21@gmail.com>
2022-05-21 15:22:41 +0800

    添加文字描述

--- src/cssCode.js
+++ src/cssCode.js
@@ -1,8 +1,9 @@
 const cssCode = `
+/* 画一个皮卡丘，首先给它配个肤色 */
 .skin {
   background: #fee433;
 }
-
+/* 接下来构建鼻子 */
 .nose {
   border-style: solid;
   border-width: 12px;
@@ -11,7 +12,7 @@ const cssCode = `
   margin-left: -12px;
   top: 58px;
 }
-
+/* 它的眼眶 */
 .eye {
   border: 2px solid black;
   width: 49px;
@@ -22,15 +23,15 @@ const cssCode = `
   border-radius: 50%;
   background: #2e2e2e;
 }
-
+/* 左眼归位 */
 .eye.left {
   transform: translateX(-116px);
 }
-
+/* 右眼归位 */
 .eye.right {
   transform: translateX(116px);
 }
-
+/* 画丘点睛 */
 .eye::before {
   content: '';
   display: block;
@@ -41,7 +42,7 @@ const cssCode = `
   border-radius: 50%;
   margin-left: 4px;
 }
-
+/* 可爱的上嘴唇 */
 .mouth::after {
   border: 2px solid black;
   background: #fee433;
@@ -59,7 +60,7 @@ const cssCode = `
   border-left: none;
   transform: rotate(20deg);
 }
-
+/* 可爱的下嘴唇 */
 .lowerLip {
   border: 2px solid black;
   width: 300px;
@@ -70,7 +71,7 @@ const cssCode = `
   bottom: 0;
   background: #990513;
 }
-
+/* 可爱的舌头 */
 .lowerLip::after {
   width: 100px;
   height: 100px;
@@ -80,7 +81,7 @@ const cssCode = `
   margin-left: -50px;
   border-radius: 50px;
 }
-
+/* 羞羞的脸颊 */
 .cheek {
   border: 2px solid black;
   width: 68px;
@@ -91,13 +92,16 @@ const cssCode = `
   top: 108px;
   background: #fc0d1c;
 }
-
+/* 咻~ 左脸颊 */
 .cheek.left {
   transform: translateX(-160px);
 }
-
+/* 咻~ 右脸颊 */
 .cheek.right {
   transform: translateX(160px);
-}`
+}
+/* 这只皮卡丘送给你~！ */`
+
+
 
 export default cssCode
--- src/main.js
+++ src/main.js
@@ -83,4 +83,3 @@ player.init()
 
 
 
-
